7023fed3ba5dce8fe72d7f86cd6ece9339a9fe04
Alan Pedraza <alanbanano@gmail.com>
2020-05-04 21:19:33 -0500

    Cambios Sin base de datos

--- Estetica_Flask.py
+++ Estetica_Flask.py
@@ -4,12 +4,12 @@ from flask import render_template, request, redirect, url_for
 import pymysql.cursors
 
 # Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='Urb@no1125',
-                             db='Estetica',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+#connection = pymysql.connect(host='localhost',
+#                            user='root',
+#                            password='Urb@no1125',
+#                            db='Estetica',
+#                            charset='utf8mb4',
+#                            cursorclass=pymysql.cursors.DictCursor)
 
 
 app = Flask(__name__, template_folder="templates")
@@ -29,18 +29,9 @@ def inventario():
         Marca_Producto = request.form['Marca_Producto']
         Modelo_Producto = request.form['Modelo_Producto']
         Costo_Producto = request.form['Costo_Producto']
-        Existencias = request.form['Existencias']
 
-        try:
-            with connection.cursor() as cursor:
-                # Agregar un nuevo Usuario
-                cursor.execute("INSERT INTO Inventario2 (Marca_Producto, Modelo_Producto, Costo_Producto, Existencias) VALUES (%s,%s,%s,%s)",(Marca_Producto, Modelo_Producto, Costo_Producto, Existencias))
-
-            # Conexion a la BD para Registro
-            connection.commit()
         finally:
             print("Se Guardo")
-
         return render_template("index.html")     
 
 @app.route('/Ventas',)
@@ -62,16 +53,6 @@ def clientes():
         Apellido_Cliente = request.form['Apellido_Cliente']
         Telefono_Cliente = request.form['Telefono_Cliente']
         Cumpleaños_Cliente = request.form['Cumpleaños_Cliente']
-
-        try:
-            with connection.cursor() as cursor:
-                # Agregar un nuevo Usuario
-                cursor.execute("INSERT INTO Clientes2 (Nombre_Cliente, Apellido_Cliente, Telefono_Cliente, Cumpleanos_Cliente) VALUES (%s,%s,%s,%s)",(Nombre_Cliente, Apellido_Cliente, Telefono_Cliente, Cumpleaños_Cliente))
-
-            # Conexion a la BD para Registro
-            connection.commit()
-        finally:
-            print("Se Guardo")
 
         return render_template("index.html")        
 
